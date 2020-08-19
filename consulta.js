8976e955bc30fee4cbecbe981cf51587cad80313
edisonmanzano200@outlook.com <edisonmanzano200@outlook.com>
2020-08-19 15:16:20 -0500

    .

--- consulta.js
+++ consulta.js
@@ -155,6 +155,12 @@ function carrito() {
     }
 }
 
+function subtotal(){
+    var cantidad = document.getElementById("cantidad").value;
+    var precio = document.getElementById("precio").value;
+    var subt = cantidad * precio;
+    document.getElementById("subtotal").value = subt;
+}
 
 function enviarReporte() {
     const nombres = [];
